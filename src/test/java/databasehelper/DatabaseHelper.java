e158e402dee326b95e768cd0b5a9f952d4de9304
Evgenii Semenov <eo.semenoff@gmail.com>
2021-08-13 15:55:21 +0300

    DatabaseHelper Class modified

--- src/test/java/databasehelper/DatabaseHelper.java
+++ src/test/java/databasehelper/DatabaseHelper.java
@@ -3,14 +3,15 @@ package databasehelper;
 import data.DataGenerator;
 import lombok.Data;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 @Data
 
 public class DatabaseHelper {
-//    String userID = String.valueOf(getUserIdByLogin(String login));
-
 
     public static void createRandomUser() throws SQLException {
         var runner = new QueryRunner();
@@ -21,22 +22,24 @@ public class DatabaseHelper {
         }
     }
 
-    public static int getUserIdByLogin(String login) throws SQLException{
+    public static String getUserIdByLogin(String login) throws SQLException{
         var runner = new QueryRunner();
         var userSQL = "SELECT id FROM users WHERE login='" + login + "';";
         try (var conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "user", "pass"            );
         ) {
-            var userID = runner.execute(conn, userSQL);
+            var userResult = runner.query(conn, userSQL, new ScalarHandler<>());
+            String userID = userResult.toString();
             return userID;
         }
     }
 
-    public static int getAuthCodeByLogin(String login) throws SQLException {
+    public static String getAuthCodeByLogin(String login) throws SQLException {
          var runner = new QueryRunner();
          var authSQL = "SELECT code FROM auth_codes WHERE user_id='" + getUserIdByLogin(login) + "' ORDER BY id DESC LIMIT 1;";
          try (var conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "user", "pass");
          ) {
-             var authCode = runner.execute(conn, authSQL);
+             var authResult = runner.query(conn, authSQL, new ScalarHandler<>());
+             String authCode = authResult.toString();
              return authCode;
          }
      }
