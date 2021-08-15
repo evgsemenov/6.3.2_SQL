21f27c7929b92473c771509e105a42e0e33b0995
Evgenii Semenov <eo.semenoff@gmail.com>
2021-08-15 18:50:08 +0300

    ClearDatabase method added to DatabaseManager

--- src/test/java/databasehelper/DatabaseManager.java
+++ src/test/java/databasehelper/DatabaseManager.java
@@ -1,28 +1,28 @@
 package databasehelper;
 
 import data.DataGenerator;
-import lombok.Data;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
-@Data
+import java.sql.Statement;
+
 public class DatabaseManager {
 
     public static void createRandomUser() throws SQLException {
         var runner = new QueryRunner();
-        var dataSQL ="INSERT INTO users(id, login, password) VALUES (?, ?, ?);";
-        try (var conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "user", "pass"            );
+        var dataSQL = "INSERT INTO users(id, login, password) VALUES (?, ?, ?);";
+        try (var conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "user", "pass");
         ) {
             runner.update(conn, dataSQL, DataGenerator.getRandomUserId(), DataGenerator.getRandomLogin(), DataGenerator.getRandomPassword());
         }
     }
 
-    public static String getUserIdByLogin(String login) throws SQLException{
+    public static String getUserIdByLogin(String login) throws SQLException {
         var runner = new QueryRunner();
         var userSQL = "SELECT id FROM users WHERE login='" + login + "';";
-        try (var conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "user", "pass"            );
+        try (var conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "user", "pass");
         ) {
             var userResult = runner.query(conn, userSQL, new ScalarHandler<>());
             String userID = userResult.toString();
@@ -31,13 +31,28 @@ public class DatabaseManager {
     }
 
     public static String getAuthCodeByLogin(String login) throws SQLException {
-                 var runner = new QueryRunner();
-                 var authSQL = "SELECT code FROM auth_codes WHERE user_id='" + getUserIdByLogin(login) + "' ORDER BY id DESC LIMIT 1;";
-                 try (var conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "user", "pass");
-                 ) {
-                     var authResult = runner.query(conn, authSQL, new ScalarHandler<>());
-                     String authCode = authResult.toString();
-                     return authCode;
-         }
-     }
+        var runner = new QueryRunner();
+        var authSQL = "SELECT code FROM auth_codes WHERE user_id='" + getUserIdByLogin(login) + "' ORDER BY id DESC LIMIT 1;";
+        try (var conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "user", "pass");
+        ) {
+            var authResult = runner.query(conn, authSQL, new ScalarHandler<>());
+            String authCode = authResult.toString();
+            return authCode;
+        }
+    }
+
+    public static void clearDatabase() {
+        try (var conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "user", "pass");
+             Statement stmt = conn.createStatement();) {
+            var clearSQL =  "SET FOREIGN_KEY_CHECKS = 0;" +
+                            "TRUNCATE TABLE auth_codes;" +
+                            "TRUNCATE TABLE card_transactions;" +
+                            "TRUNCATE TABLE cards;" +
+                            "TRUNCATE TABLE users;" +
+                            "SET FOREIGN_KEY_CHECKS = 0;";
+            stmt.executeUpdate(clearSQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/test/java/loginTest/InternetBankLoginTest.java
+++ src/test/java/loginTest/InternetBankLoginTest.java
@@ -1,7 +1,9 @@
 package loginTest;
 
 import data.DataGenerator;
+import data.UsersInfo;
 import databasehelper.DatabaseManager;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,31 +15,34 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class InternetBankLoginTest {
-    String login = "vasya";
-    String password = "qwerty123";
 
     @BeforeEach
     void browserSetup() {
         open("http://localhost:9999");
     }
 
+    @AfterAll
+    static void dbTearDown() {
+        DatabaseManager.clearDatabase();
+    }
+
     @Test
     void shouldLoginActiveUserWithAuthCode() throws SQLException {
-        $("[data-test-id='login']").$("[class='input__control']").setValue(login);
-        $("[data-test-id='password']").$("[class='input__control']").setValue(password);
+        $("[data-test-id='login']").$("[class='input__control']").setValue(UsersInfo.activeUserLogin);
+        $("[data-test-id='password']").$("[class='input__control']").setValue(UsersInfo.activeUserPassword);
         $("[data-test-id='action-login']").click();
         $("[id='root']").$("[class='paragraph paragraph_theme_alfa-on-white'")
                 .shouldHave(exactText("Необходимо подтверждение")).shouldBe(visible);
         $("[data-test-id='code']").$("[class='input__control']").
-                setValue(DatabaseManager.getAuthCodeByLogin(login));
+                setValue(DatabaseManager.getAuthCodeByLogin(UsersInfo.activeUserLogin));
         $("[data-test-id='action-verify']").click();
         $("[data-test-id='dashboard']").shouldBe(visible).shouldHave(exactText("Личный кабинет"));
     }
 
     @Test
-    void shouldGetErrorIfWrongAuthCodeSentThreeTimes() throws SQLException {
-        $("[data-test-id='login']").$("[class='input__control']").setValue(login);
-        $("[data-test-id='password']").$("[class='input__control']").setValue(password);
+    void shouldGetErrorIfWrongAuthCodeSentThreeTimes() {
+        $("[data-test-id='login']").$("[class='input__control']").setValue(UsersInfo.activeUserLogin);
+        $("[data-test-id='password']").$("[class='input__control']").setValue(UsersInfo.activeUserPassword);
         $("[data-test-id='action-login']").click();
         $("[data-test-id='code']").$("[class='input__control']")
                 .setValue(DataGenerator.getRandomAuthCode());
@@ -53,9 +58,9 @@ public class InternetBankLoginTest {
     }
 
     @Test
-    void shouldReturnToMainPageIfWrongAuthCodeSentThreeTimes() throws SQLException {
-        $("[data-test-id='login']").$("[class='input__control']").setValue(login);
-        $("[data-test-id='password']").$("[class='input__control']").setValue(password);
+    void shouldReturnToMainPageIfWrongAuthCodeSentThreeTimes() {
+        $("[data-test-id='login']").$("[class='input__control']").setValue(UsersInfo.activeUserLogin);
+        $("[data-test-id='password']").$("[class='input__control']").setValue(UsersInfo.activeUserPassword);
         $("[data-test-id='action-login']").click();
         $("[data-test-id='code']").$("[class='input__control']")
                 .setValue(DataGenerator.getRandomAuthCode());
@@ -66,14 +71,14 @@ public class InternetBankLoginTest {
         $("[data-test-id='code']").$("[class='input__control']")
                 .setValue(DataGenerator.getRandomAuthCode());
         $("[data-test-id='action-verify']").click();
-        $("[id='root']").$("[class='paragraph paragraph_theme_alfa-on-white'").shouldBe(visible)
+        $("[id='root']").$("[class='paragraph paragraph_theme_alfa-on-white']").shouldBe(visible)
                 .shouldHave(exactText("Мы гарантируем безопасность ваших данных"));
     }
 
 
     @Test
     void shouldGetErrorIfWrongPassword() {
-        $("[data-test-id='login']").$("[class='input__control']").setValue(login);
+        $("[data-test-id='login']").$("[class='input__control']").setValue(UsersInfo.activeUserLogin);
         $("[data-test-id='password']").$("[class='input__control']").setValue(DataGenerator.getRandomPassword());
         $("[data-test-id='action-login']").click();
         $("[data-test-id='error-notification']").shouldBe(visible)
@@ -83,7 +88,7 @@ public class InternetBankLoginTest {
     @Test
     void shouldGetErrorIfWrongLogin() {
         $("[data-test-id='login']").$("[class='input__control']").setValue(DataGenerator.getRandomLogin());
-        $("[data-test-id='password']").$("[class='input__control']").setValue(password);
+        $("[data-test-id='password']").$("[class='input__control']").setValue(UsersInfo.activeUserPassword);
         $("[data-test-id='action-login']").click();
         $("[data-test-id='error-notification']").shouldBe(visible)
                 .shouldHave(exactText("Ошибка\n" + "Ошибка! Неверно указан логин или пароль"));
@@ -101,8 +106,8 @@ public class InternetBankLoginTest {
 
     @Test
     void shouldGetErrorIfWrondAuthCode() {
-        $("[data-test-id='login']").$("[class='input__control']").setValue(login);
-        $("[data-test-id='password']").$("[class='input__control']").setValue(password);
+        $("[data-test-id='login']").$("[class='input__control']").setValue(UsersInfo.activeUserLogin);
+        $("[data-test-id='password']").$("[class='input__control']").setValue(UsersInfo.activeUserPassword);
         $("[data-test-id='action-login']").click();
         $("[data-test-id='code']").$("[class='input__control']").setValue(DataGenerator.getRandomAuthCode());
         $("[data-test-id='action-verify']").click();
@@ -112,8 +117,8 @@ public class InternetBankLoginTest {
 
     @Test
     void shouldRequireFilledFieldIfEmptyAuthCodeField() {
-        $("[data-test-id='login']").$("[class='input__control']").setValue(login);
-        $("[data-test-id='password']").$("[class='input__control']").setValue(password);
+        $("[data-test-id='login']").$("[class='input__control']").setValue(UsersInfo.activeUserLogin);
+        $("[data-test-id='password']").$("[class='input__control']").setValue(UsersInfo.activeUserPassword);
         $("[data-test-id='action-login']").click();
         $("[data-test-id='action-verify']").click();
         $("[data-test-id='code']").$("[class='input__sub']").shouldBe(visible)
